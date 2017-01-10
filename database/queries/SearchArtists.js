466589d5bc8728768cef4f6bf017bf594b9d769a
mong991 <dogcat614@gmail.com>
2017-01-10 17:11:36 +0800

    build search Query

--- database/queries/SearchArtists.js
+++ database/queries/SearchArtists.js
@@ -12,10 +12,7 @@ const Artist = require('../models/artist');
 module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
   //write a query that will follow sort, offset, limit, options only
 
-  // const sortOrder = {};
-  // sortOrder[sortProperty] = 1;
-
-  const query = Artist.find({})
+  const query = Artist.find(buildQuery(criteria))
     .sort({ [sortProperty]: 1 })
     .skip(offset)
     .limit(limit);
@@ -29,4 +26,28 @@ module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
         limit
       };
     });
+};
+
+const buildQuery = (criteria) => {
+  const query = {};
+
+  if (criteria.age) {
+    query.age = {
+      $gte: criteria.age.min,
+      $lte: criteria.age.max
+    };
+  }
+
+  if (criteria.yearsActive) {
+    query.yearsActive = {
+      $gte: criteria.yearsActive.min,
+      $lte: criteria.yearsActive.max
+    };
+  }
+
+  if (criteria.name) {
+    query.$text = { $search: criteria.name };
+  }
+
+  return query;
 };
