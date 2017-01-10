42ddd0ddd8dabe4f7b254926ee928b05558051f3
mong991 <dogcat614@gmail.com>
2017-01-10 15:18:53 +0800

    creatArtist

--- database/queries/CreateArtist.js
+++ database/queries/CreateArtist.js
@@ -6,4 +6,8 @@ const Artist = require('../models/artist');
  * @return {promise} A promise that resolves with the Artist that was created
  */
 module.exports = (artistProps) => {
+  //mew User({ name: 'Joe'} );
+  const artist = new Artist(artistProps);
+
+  return artist.save();
 };
